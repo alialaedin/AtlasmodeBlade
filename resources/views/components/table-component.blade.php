10c7cb077742920bb2fc798d34619383cd9f9367
ali alaeddin <alialaedin1383@gmail.com>
2025-05-03 20:14:53 +0330

    fix bugs

--- resources/views/components/table-component.blade.php
+++ resources/views/components/table-component.blade.php
@@ -1,9 +1,9 @@
-<div class="table-responsive table-component">
+<div class="table-responsive">
     <table
         class="table table-striped table-bordered text-nowrap text-center {{ $class ?? ''}}"
         @isset($id) id="{{ $id }}" @endisset
     >
-        <thead class="border-top" style="position: sticky;">
+        <thead class="border-top">
         {{$tableTh}}
         </thead>
         <tbody @isset($idTbody) id="{{ $idTbody }}" @endisset>
